b6520496789b3971de274aa727e9895d10923fbf
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-03-31 10:06:33 +0200

    premier step

---
+++ app/src/main/java/education/cccp/tp11listview/MainActivity.java
@@ -0,0 +1,14 @@
+package education.cccp.tp11listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/education/cccp/tp11listview/model/Person.java
@@ -0,0 +1,7 @@
+package education.cccp.tp11listview.model;
+
+public class Person {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+}
