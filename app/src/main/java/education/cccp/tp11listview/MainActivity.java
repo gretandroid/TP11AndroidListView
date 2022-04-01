f8758ee92300bb7037ba9c06bb941be6ad2a2d51
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-04-01 22:10:32 +0200

    update UI, stepping to the goal.

--- app/src/main/java/education/cccp/tp11listview/MainActivity.java
+++ app/src/main/java/education/cccp/tp11listview/MainActivity.java
@@ -5,15 +5,16 @@ import static android.widget.Toast.makeText;
 import static education.cccp.tp11listview.R.id.editTextPersonFirstNameId;
 import static education.cccp.tp11listview.R.id.editTextPersonLastNameId;
 import static education.cccp.tp11listview.R.layout.activity_main;
-import static education.cccp.tp11listview.SecondActivity.PERSON_KEY;
-import static education.cccp.tp11listview.controller.PersonDao.addPerson;
-import static education.cccp.tp11listview.controller.PersonDao.getAllPersons;
+import static education.cccp.tp11listview.SecondActivity.CURRENT_PERSON_KEY;
+import static education.cccp.tp11listview.repositories.PersonDao.findAll;
+import static education.cccp.tp11listview.repositories.PersonDao.save;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,17 +22,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.io.Serializable;
 import java.util.Objects;
 
-import education.cccp.tp11listview.model.Person;
+import education.cccp.tp11listview.models.Person;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String PERSONS_KEY = "persons_key";
+    public static final String PERSON_LIST_KEY = "person_list_key";
+    public static final int OUT_OF_BOUND_INDEX = -1;
+    public static final String EMPTY_FIELD = "";
 
     private EditText editTextPersonFirstName;
     private EditText editTextPersonLastName;
 
+    private int indiceSelected = OUT_OF_BOUND_INDEX;
     // declare a launcher to call an intent to start
     // an execution process ActivityResultContracts
-    private ActivityResultLauncher<Intent> intentLauncher;
+    private ActivityResultLauncher<Intent> intentActivityResultLauncher;
+
+    private void initializePersonFields(String firstName, String lastName) {
+        editTextPersonFirstName.setText(firstName);
+        editTextPersonLastName.setText(lastName);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,30 +48,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(activity_main);
         editTextPersonFirstName = findViewById(editTextPersonFirstNameId);
         editTextPersonLastName = findViewById(editTextPersonLastNameId);
-        intentLauncher = registerForActivityResult(
+        intentActivityResultLauncher = registerForActivityResult(
                 new StartActivityForResult(),
-                result -> {
-                    if (result.getResultCode() == RESULT_OK) {
-                        Person person = (Person) Objects.requireNonNull(
-                                result.getData())
-                                .getSerializableExtra(PERSON_KEY);
-                        makeText(getBaseContext(),
-                                person.toString(),
-                                LENGTH_LONG).show();
+                (ActivityResult activityResult) -> {
+                    Intent data = activityResult.getData();
+                    if (activityResult.getResultCode() == RESULT_OK) {
+                        Person person = (Person) Objects.requireNonNull(data)
+                                .getSerializableExtra(CURRENT_PERSON_KEY);
+                        initializePersonFields(person.getFirstName(),
+                                person.getLastName());
+
                     }
                 }
         );
     }
 
-    public void onClickSaveButtonEvent(View view) throws Exception {
-        addPerson(new Person(editTextPersonFirstName
-                .getText()
-                .toString(),
-                editTextPersonLastName
-                        .getText()
-                        .toString()));
-        intentLauncher.launch(new Intent(this,
-                SecondActivity.class).putExtra(PERSONS_KEY,
-                (Serializable) getAllPersons()));
+    public void onClickCreateButtonEvent(View view) throws Exception {
+        makeText(this,
+                save(new Person(editTextPersonFirstName.getText().toString(),
+                        editTextPersonLastName.getText().toString())).toString(),
+                LENGTH_LONG)
+                .show();
+        initializePersonFields(EMPTY_FIELD,
+                EMPTY_FIELD);
+    }
+
+    public void onClickEditButtonEvent(View view) {
+    }
+
+    public void onClickDeleteButtonEvent(View view) {
+    }
+
+    public void onClickShowAllButtonEvent(View view) {
     }
 }
--- app/src/main/java/education/cccp/tp11listview/SecondActivity.java
+++ app/src/main/java/education/cccp/tp11listview/SecondActivity.java
@@ -1,13 +1,13 @@
 package education.cccp.tp11listview;
 
 import static android.R.layout.simple_list_item_1;
-import static education.cccp.tp11listview.MainActivity.PERSONS_KEY;
+import static education.cccp.tp11listview.MainActivity.PERSON_LIST_KEY;
 import static education.cccp.tp11listview.R.id.personListViewId;
 import static education.cccp.tp11listview.R.layout.activity_second;
+import static education.cccp.tp11listview.repositories.PersonDao.findAll;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -17,44 +17,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.List;
 
-import education.cccp.tp11listview.controller.PersonDao;
-import education.cccp.tp11listview.model.Person;
+import education.cccp.tp11listview.models.Person;
 
 public class SecondActivity extends AppCompatActivity {
 
-    public static final String PERSON_KEY = "person_key";
-
-    private void logPersons() {
-        //noinspection unchecked
-        Log.d(SecondActivity.class.getSimpleName(),
-                ((List<Person>) getIntent().getSerializableExtra(PERSONS_KEY))
-                        .toArray()[0]
-                        .toString());
-    }
+    public static final String CURRENT_PERSON_KEY = "current_person_key";
+    public static final String CURRENT_PERSON_INDEX_KEY = "current_person_index_key";
+    private Person currentPerson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(activity_second);
         ListView personsListView = findViewById(personListViewId);
-        logPersons();
         //noinspection unchecked
         personsListView.setAdapter(new ArrayAdapter<>(this,
                 simple_list_item_1,
                 (List<Person>) getIntent()
-                        .getSerializableExtra(PERSONS_KEY)));
+                        .getSerializableExtra(PERSON_LIST_KEY)));
 
         personsListView.setOnItemClickListener((AdapterView<?> adapterView,
                                                 View view,
                                                 int index,
                                                 long l) -> {
             //retrieve person's clicked
-            setResult(RESULT_OK,
-                    new Intent().putExtra(
-                            PERSON_KEY,
-                            PersonDao.getAllPersons()
-                                    .get(index)
-                    ));
+            setResult(RESULT_OK, new Intent().putExtra(
+                    CURRENT_PERSON_KEY,
+                    findAll().get(index)));
             finish();
         });
     }
